701fc67092f21b1fc8ca25f1cf44d8f4b70396d0
Kien Nguyen <kiennt2609@gmail.com>
2023-04-04 09:58:11 +0700

    init: start coding

--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-module github.com/charmbracelet/bubbletea-app-template
+module github.com/ntk148v/ggnf
 
 go 1.19
 
@@ -6,11 +6,15 @@ require (
 	github.com/charmbracelet/bubbles v0.15.0
 	github.com/charmbracelet/bubbletea v0.23.2
 	github.com/charmbracelet/lipgloss v0.7.1
+	github.com/google/go-github v17.0.0+incompatible
 )
 
 require (
+	github.com/atotto/clipboard v0.1.4 // indirect
 	github.com/aymanbagabas/go-osc52/v2 v2.0.1 // indirect
 	github.com/containerd/console v1.0.3 // indirect
+	github.com/google/go-cmp v0.5.9 // indirect
+	github.com/google/go-querystring v1.1.0 // indirect
 	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
 	github.com/mattn/go-isatty v0.0.17 // indirect
 	github.com/mattn/go-localereader v0.0.1 // indirect
@@ -20,8 +24,9 @@ require (
 	github.com/muesli/reflow v0.3.0 // indirect
 	github.com/muesli/termenv v0.15.1 // indirect
 	github.com/rivo/uniseg v0.2.0 // indirect
+	github.com/sahilm/fuzzy v0.1.0 // indirect
 	golang.org/x/sync v0.1.0 // indirect
 	golang.org/x/sys v0.6.0 // indirect
-	golang.org/x/term v0.0.0-20210927222741-03fcf44c2211 // indirect
-	golang.org/x/text v0.3.8 // indirect
+	golang.org/x/term v0.6.0 // indirect
+	golang.org/x/text v0.8.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,75 +1,165 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"os"
+	"path/filepath"
+	"strings"
 
-	"github.com/charmbracelet/bubbles/key"
-	"github.com/charmbracelet/bubbles/spinner"
+	"github.com/charmbracelet/bubbles/list"
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
+	"github.com/google/go-github/github"
 )
 
-type errMsg error
+var (
+	cacheFile string
+	items     []list.Item
+	docStyle  = lipgloss.NewStyle().Margin(1, 2)
+)
 
-type model struct {
-	spinner  spinner.Model
-	quitting bool
-	err      error
+type item struct {
+	name      string
+	url       string
+	latest    string
+	installed string
 }
 
-var quitKeys = key.NewBinding(
-	key.WithKeys("q", "esc", "ctrl+c"),
-	key.WithHelp("", "press q to quit"),
-)
+func (i item) Title() string { return i.name }
+func (i item) Description() string {
+	return fmt.Sprintf("Installed version: %s - Latest version: %s", i.installed, i.latest)
+}
+func (i item) LatestVersion() string  { return i.latest }
+func (i item) CurrentVersion() string { return i.installed }
+func (i item) DownloadURL() string    { return i.url }
+func (i item) FilterValue() string    { return i.name }
 
-func initialModel() model {
-	s := spinner.New()
-	s.Spinner = spinner.Dot
-	s.Style = lipgloss.NewStyle().Foreground(lipgloss.Color("205"))
-	return model{spinner: s}
+type model struct {
+	list list.Model
 }
 
 func (m model) Init() tea.Cmd {
-	return m.spinner.Tick
+	return nil
 }
 
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
-
 	case tea.KeyMsg:
-		if key.Matches(msg, quitKeys) {
-			m.quitting = true
+		if msg.String() == "ctrl+c" {
 			return m, tea.Quit
-
 		}
-		return m, nil
-	case errMsg:
-		m.err = msg
-		return m, nil
-
-	default:
-		var cmd tea.Cmd
-		m.spinner, cmd = m.spinner.Update(msg)
-		return m, cmd
+	case tea.WindowSizeMsg:
+		h, v := docStyle.GetFrameSize()
+		m.list.SetSize(msg.Width-h, msg.Height-v)
 	}
+
+	var cmd tea.Cmd
+	m.list, cmd = m.list.Update(msg)
+	return m, cmd
 }
 
 func (m model) View() string {
-	if m.err != nil {
-		return m.err.Error()
-	}
-	str := fmt.Sprintf("\n\n   %s Loading forever... %s\n\n", m.spinner.View(), quitKeys.Help().Desc)
-	if m.quitting {
-		return str + "\n"
-	}
-	return str
+	return docStyle.Render(m.list.View())
 }
 
 func main() {
-	p := tea.NewProgram(initialModel())
+	err := createCacheFile()
+	if err != nil {
+		panic(err)
+	}
+
+	// Create context
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	// Get the latest release Nerd Fonts
+	if err := getLatestRelease(ctx); err != nil {
+		panic(err)
+	}
+
+	m := model{list: list.New(items, list.NewDefaultDelegate(), 0, 0)}
+	m.list.Title = "GGNF"
+
+	p := tea.NewProgram(m, tea.WithAltScreen())
+
 	if _, err := p.Run(); err != nil {
-		fmt.Println(err)
+		fmt.Println("Error running program:", err)
 		os.Exit(1)
 	}
+}
+
+// createCacheFile
+func createCacheFile() error {
+	cacheDir, err := os.UserCacheDir()
+	if err != nil {
+		return err
+	}
+
+	cacheFile = filepath.Join(cacheDir, "ggnf.json")
+	if _, err := os.Stat(cacheFile); err != nil {
+		if os.IsNotExist(err) {
+			os.Create(cacheFile)
+			return setCache(map[string]string{})
+		}
+		return err
+	}
+
+	return nil
+}
+
+// getCache loads cache data from cache file
+func getCache() (map[string]string, error) {
+	var installed map[string]string
+	raw, err := ioutil.ReadFile(cacheFile)
+	if err != nil {
+		return installed, nil
+	}
+
+	err = json.Unmarshal(raw, &installed)
+	return installed, err
+}
+
+// setCache saves the input to cache file
+func setCache(installed map[string]string) error {
+	raw, err := json.Marshal(installed)
+	if err != nil {
+		return err
+	}
+
+	return ioutil.WriteFile(cacheFile, raw, 0644)
+}
+
+// getLatestRelease fetches Github for the latest Nerd Fonts release
+func getLatestRelease(ctx context.Context) error {
+	// Load installed fonts
+	installed, err := getCache()
+	if err != nil {
+		return err
+	}
+
+	// Github client
+	client := github.NewClient(nil)
+	latestRelease, _, err := client.Repositories.GetLatestRelease(ctx, "ryanoasis", "nerd-fonts")
+	if err != nil {
+		return err
+	}
+
+	for _, a := range latestRelease.Assets {
+		i := item{
+			name:      strings.Trim(*a.Name, ".zip"),
+			url:       *a.BrowserDownloadURL,
+			latest:    *latestRelease.Name,
+			installed: "None",
+		}
+
+		if v, ok := installed[i.name]; ok {
+			i.installed = v
+		}
+
+		items = append(items, i)
+	}
+	return nil
 }
